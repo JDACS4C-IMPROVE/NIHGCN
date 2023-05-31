6e7889f83756e6f0f6b830207cee55bf87388213
Nicholas Chia <chuh.chia@gmail.com>
2023-05-31 11:39:45 -0500

    Update candle_glue.sh

--- candle_glue.sh
+++ candle_glue.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -a
-export PYTHONPATH=$IMPROVE_MODEL_DIR
+export PYTHONPATH=$IMPROVE_MODEL_DIR:$PYTHONPATH
 set +a
 
 if [ -z "$CANDLE_DATA_DIR" ]; then
@@ -11,7 +11,7 @@ fi
 
 mkdir -p $CANDLE_DATA_DIR/NIHGCN/Data
 
-python $PYTHONPATH/get_test_data.py
+python get_test_data.py
 
 #move directories around - use GDSC as test case
 mv $CANDLE_DATA_DIR/common/Data/GDSC/*.csv $CANDLE_DATA_DIR/NIHGCN/Data/.
